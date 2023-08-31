29af3de3be53f6089b7794bedb4b8c3bc29ea0bf
kobdash <102815208+kobdash@users.noreply.github.com>
2023-08-31 20:34:57 +0200

    updated the slide

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -4,7 +4,7 @@ function ContactPage() {
   return (
     <div>
       <h1>Contact Page</h1>
-      {/* Add your contact information or form here */}
+      {<h3>Email: <a href="mailto:jefftny1@gmail.com"> jefftny1@gmail.com</a></h3>}
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Typewriter from './Typewriter';
 import CarouselSlider from './CarouselSlider';
 import SlideData from './SlideData';
+import './styles/HomePage.css';
 
 function HomePage() {
   return (
     <div>
-      <h1><Typewriter text="Hi, I'm Jeff Nyahuye." delay={50} /></h1>
+       <div class="block-container">
+        <div class="block-content">
+        <h1><Typewriter text="Hi, I'm Jeff Nyahuye." delay={50} /></h1>
       <h2>I am a full stack developer and IT professional.</h2>
       <p>I love crafting captivating websites, coding dynamic programs, building interactive applications and enhancing existing projects.</p>
+      
+        </div>
+    </div>
+
+
+      
       
       {<CarouselSlider slides={SlideData} />}
       
