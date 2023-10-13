a26c05cc95c0f603228dc18a1a69b6e22ee5cb50
kobdash <jefftny1@gmail.com>
2023-10-13 12:06:46 +0200

    updated my menu bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './components/styles/_custom-bootstrap.scss';
-import { CSSTransition, TransitionGroup } from 'react-transition-group'; // Import TransitionGroup
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import HomePage from './components/HomePage';
 import PortfolioPage from './components/PortfolioPage';
 import AboutPage from './components/AboutPage';
@@ -10,25 +10,35 @@ import Navbar from './components/Navbar';
 import './components/styles/App.css';
 
 function App() {
+  const [navbarVisible, setNavbarVisible] = useState(false); // Start initially hidden
+
+  const toggleNavbar = () => {
+    setNavbarVisible(!navbarVisible);
+  };
+
   return (
     <Router>
       <div className="bg-dark text-light">
-        <Navbar />
+      <button onClick={toggleNavbar} class="btn btn-secondary dropdown-toggle">Menu
+</button>
+        <div className={`navbar-container ${navbarVisible ? 'visible' : ''}`}>
+          <Navbar />
+        </div>
         <Route
           render={({ location }) => (
-            <TransitionGroup> {/* Wrap the routes in TransitionGroup */}
+            <TransitionGroup>
               <CSSTransition
-                key={location.key}
-                classNames="slide"
-                timeout={1000} // Adjust the timeout as needed
-              >
-                <Switch location={location}>
-                  <Route path="/" exact component={HomePage} />
-                  <Route path="/portfolio" component={PortfolioPage} />
-                  <Route path="/about" component={AboutPage} />
-                  <Route path="/contact" component={ContactPage} />
-                </Switch>
-              </CSSTransition>
+  key={location.key}
+  classNames={location.pathname === '/' ? 'initial-slide' : 'slide'}
+  timeout={1000}
+>
+  <Switch location={location}>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/portfolio" component={PortfolioPage} />
+    <Route path="/about" component={AboutPage} />
+    <Route path="/contact" component={ContactPage} />
+  </Switch>
+</CSSTransition>
             </TransitionGroup>
           )}
         />
